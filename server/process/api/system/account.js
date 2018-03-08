53087f766dcddc3831f16d6176a4fe821e34ac87
Anlibraly <anlibraly@gmail.com>
2018-03-08 19:49:15 +0800

    update

--- server/process/api/system/account.js
+++ server/process/api/system/account.js
@@ -48,11 +48,12 @@ module.exports = ( router ) => {
 			return;			
 		}
 		let url = `https://api.weixin.qq.com/sns/jscode2session?appid=wxc322fe742afc756b&secret=b0bb57153552e5b3144e7b71ffbecf90&js_code=${this.params.code}&grant_type=authorization_code`;
-		
+		let that = this;
+
 		yield new Promise(function(resolve, reject) {
 			request(url, (err, res, body) => {
 				if(err){
-					this.body = {
+					that.body = {
 						result : err,
 						res : {
 							status : false
@@ -61,7 +62,7 @@ module.exports = ( router ) => {
 					resolve();	
 				}
 	
-				this.body = body;
+				that.body = body;
 				resolve();
 			});	
 		});	
